8d4dd70d3b4617b6099a03c6ccdef6d9ff05d133
its-a-setup <96050384+its-a-setup@users.noreply.github.com>
2023-04-20 16:03:28 +0700

    add data to xcoms and run the same task n times

--- dags/create_activity_dag.py
+++ dags/create_activity_dag.py
@@ -7,7 +7,6 @@ from airflow.operators.bash import BashOperator
 
 #TODO authenticate to github repo, git add. and git commit the file with dot
 #TODO do it N times based on mimic_n_commits function
-#TODO change execution interval to daily
 
 default_args={
     'owner' : 'active_developer',
@@ -15,42 +14,37 @@ default_args={
     'retry_interval' : timedelta(minutes=2)
 }
 
-def mimic_n_commits():
-    #n_commits=random.randint(1,10)
-    n_commits=1
+# def get_n_of_commits(ti):
+#     n_commits=random.randint(1,10)
+#     ti.xcom_push(key="n_commits", value=n_commits)
+
+def make_change_to_file():
     with open('dags/git_dir/file_to_push.txt', 'a') as file_with_updates:
-        file_with_updates.write(n_commits*'.')     
+        file_with_updates.write('.')     
     print(f'Wow, you\'ve done a change to the file, added {n_commits} dots')
 
 with DAG(
-    dag_id='mimic_activity_v8',
+    dag_id='mimic_activity_v12',
     default_args=default_args,
-    start_date=datetime(2023,4, 18),
+    start_date=datetime(2023,4, 19),
     schedule_interval='@daily'
 ) as dag:
-    task1=PythonOperator(
-        task_id = 'test_operator',
-        python_callable=mimic_n_commits
-    )
 
-    task2=BashOperator(
-        task_id = 'navigate_to_dir',
-        bash_command = 'ls'
-    )
+    chain_operators=[]
+    n_commits=1
 
-    task3=BashOperator(
-        task_id = 'add_file_to_staging',
-        bash_command = 'git add .'
-    )
+    for n in range(n_commits):
+        task1=PythonOperator(
+        task_id = f'make_{n}th_change_to_file',
+        python_callable=make_change_to_file
+        )
+        chain_operators.append(task1)
 
-    task4=BashOperator(
-        task_id = 'commit_file',
-        bash_command = 'git commit -m "does it work?"'
-    )
+        task2=BashOperator(
+            task_id = f'test_call_{n}_out_of_{n_commits}',
+            bash_command = 'pwd; sudo mkdir $active_dev/test_dir'
+        )
+        chain_operators.append(task2)
 
-    task5=BashOperator(
-        task_id = 'push_file_to_remote_repo',
-        bash_command = 'git push -u origin main'
-    )
-
-    task1>>task2
+    for i, val in enumerate(chain_operators[:-1]):
+        val.set_downstream(chain_operators[i+1])
