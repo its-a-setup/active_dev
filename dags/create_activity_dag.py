2af5a49f49ea8026c8e1f6ce1fe4664a96a0e095
its-a-setup <96050384+its-a-setup@users.noreply.github.com>
2023-04-24 15:23:34 +0700

    completed TODOs

--- dags/create_activity_dag.py
+++ dags/create_activity_dag.py
@@ -1,14 +1,6 @@
-import os
-from dotenv import load_dotenv
 from datetime import datetime
-
-from github import Github
 from airflow.decorators import task, dag
 
-#TODO change create_file to update_file
-#TODO find proper way to name files uniquely
-#TODO get the name of repo without relying on order
-#TODO delete previous file when new one was committed, to save space
 
 default_args={
     'owner' : 'active_developer',
@@ -24,26 +16,37 @@ def get_n_of_commits():
 
 @task
 def do_n_commits(n):
-    import random
+    import os
+    from dotenv import load_dotenv
+    from github import Github
     import time
+
     load_dotenv()
 
     GITHUB_ACCESS_TOKEN=os.getenv('GITHUB_ACCESS_TOKEN')
+    GITHUB_USERNAME=os.getenv('GITHUB_USERNAME')
+    GITHUB_REPO=os.getenv('GITHUB_REPO')
+    FILE_NAME=os.getenv("FILE_NAME")
+
     g = Github(GITHUB_ACCESS_TOKEN)
-    user = g.get_user()
-    my_repo= user.get_repos()[4]
+    my_repo= g.get_repo(f"{GITHUB_USERNAME}/{GITHUB_REPO}")
 
     print(f"We will be commiting to this repo: {my_repo}")
-    number_for_file=random.randint(1,10000000)
-    
-    my_repo.create_file(f"{number_for_file}-th_test_commit.txt", f"This is the {number_for_file}-th commit from airflow dag", ".")
 
-    time.sleep(300)
+    contents = my_repo.get_contents(FILE_NAME)
+    content_of_the_file = str(datetime.now())
+    commit_message = f"Commit at {content_of_the_file}"
+
+    my_repo.update_file(contents.path, commit_message, content_of_the_file, contents.sha)
+    print(f"{n} - Successfully commited the file to github repo")
+
+    print("Waiting 120 seconds to avoid clashes with commits")
+    time.sleep(120)
 
 @dag(
-    dag_id='mimic_activity_v27',
+    dag_id='mimic_activity_v34',
     default_args=default_args,
-    start_date=datetime(2023,4, 22),
+    start_date=datetime(2023,4, 23),
     schedule_interval='@daily'
 )
 
