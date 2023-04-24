18715baa49bb64afec1fc4b31225ef521042ea1b
its-a-setup <96050384+its-a-setup@users.noreply.github.com>
2023-04-24 15:39:42 +0700

    pretify code with isort and black

--- dags/create_activity_dag.py
+++ dags/create_activity_dag.py
@@ -1,35 +1,36 @@
 from datetime import datetime
-from airflow.decorators import task, dag
 
+from airflow.decorators import dag, task
 
-default_args={
-    'owner' : 'active_developer',
-    'retries' : 5
-}
+default_args = {"owner": "active_developer", "retries": 5}
+
 
 @task
 def get_n_of_commits():
     import random
+
     n = random.randint(2, 10)
-    my_list = list(range(1, n+1))
+    my_list = list(range(1, n + 1))
     return my_list
+
 
 @task
 def do_n_commits(n):
     import os
+    import time
+
     from dotenv import load_dotenv
     from github import Github
-    import time
 
     load_dotenv()
 
-    GITHUB_ACCESS_TOKEN=os.getenv('GITHUB_ACCESS_TOKEN')
-    GITHUB_USERNAME=os.getenv('GITHUB_USERNAME')
-    GITHUB_REPO=os.getenv('GITHUB_REPO')
-    FILE_NAME=os.getenv("FILE_NAME")
+    GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
+    GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
+    GITHUB_REPO = os.getenv("GITHUB_REPO")
+    FILE_NAME = os.getenv("FILE_NAME")
 
     g = Github(GITHUB_ACCESS_TOKEN)
-    my_repo= g.get_repo(f"{GITHUB_USERNAME}/{GITHUB_REPO}")
+    my_repo = g.get_repo(f"{GITHUB_USERNAME}/{GITHUB_REPO}")
 
     print(f"We will be commiting to this repo: {my_repo}")
 
@@ -37,20 +38,23 @@ def do_n_commits(n):
     content_of_the_file = str(datetime.now())
     commit_message = f"Commit at {content_of_the_file}"
 
-    my_repo.update_file(contents.path, commit_message, content_of_the_file, contents.sha)
+    my_repo.update_file(
+        contents.path, commit_message, content_of_the_file, contents.sha
+    )
     print(f"{n} - Successfully commited the file to github repo")
 
     print("Waiting 120 seconds to avoid clashes with commits")
     time.sleep(120)
 
-@dag(
-    dag_id='mimic_activity_v34',
-    default_args=default_args,
-    start_date=datetime(2023,4, 23),
-    schedule_interval='@daily'
-)
 
+@dag(
+    dag_id="mimic_activity_v34",
+    default_args=default_args,
+    start_date=datetime(2023, 4, 23),
+    schedule_interval="@daily",
+)
 def my_dag():
     do_n_commits.expand(n=get_n_of_commits())
 
-dag=my_dag()
+
+dag = my_dag()
